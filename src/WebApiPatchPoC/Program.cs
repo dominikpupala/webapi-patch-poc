4c420ff686cad552b90e53416e710f0c6fd45d11
Dominik Pupala <dominik.pupala@gmail.com>
2025-11-24 23:14:37 +0100

    add swagger as openapi ui

--- src/WebApiPatchPoC/Program.cs
+++ src/WebApiPatchPoC/Program.cs
@@ -7,10 +7,11 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.MapOpenApi();
+app.UseSwaggerUI(options =>
 {
-    app.MapOpenApi();
-}
+    options.SwaggerEndpoint("/openapi/v1.json", "v1");
+});
 
 var summaries = new[]
 {
