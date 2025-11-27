2e3c62870acbadd575b0110c52b82eb499b4f459
Dominik Pupala <dominik.pupala@gmail.com>
2025-11-27 10:08:42 +0100

    remove placeholder weather endpoint

--- src/WebApiPatchPoC/Endpoints.cs
+++ src/WebApiPatchPoC/Endpoints.cs
@@ -11,7 +11,6 @@ internal static class Endpoints
             var endpoints = app.MapGroup("api/");
 
             endpoints.MapProductsEndpoints();
-            endpoints.MapTemporaryEndpoints();
         }
     }
 
@@ -25,36 +24,5 @@ internal static class Endpoints
 
             productsGroup.MapGetProducts();
         }
-
-        public void MapTemporaryEndpoints()
-        {
-            var summaries = new[]
-            {
-                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            };
-
-            endpoints.MapGet("/weatherforecast", () =>
-            {
-#pragma warning disable CA5394 // Do not use insecure randomness
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    (
-                        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        Random.Shared.Next(-20, 55),
-                        summaries[Random.Shared.Next(summaries.Length)]
-                    ))
-                    .ToArray();
-#pragma warning restore CA5394 // Do not use insecure randomness
-                return forecast;
-            })
-            .WithName("GetWeatherForecast");
-        }
     }
 }
-
-#pragma warning disable CA1852// Temporary code
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
-#pragma warning restore CA1852
