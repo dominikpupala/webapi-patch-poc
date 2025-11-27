74cd6dad415e14136d013d7d187c2a48debd966b
Dominik Pupala <dominik.pupala@gmail.com>
2025-11-27 19:13:15 +0100

    add product domain (like)

---
+++ src/WebApiPatchPoC/Features/Products/Domain/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiPatchPoC.Features.Products.Domain;
+
+// Domain types following DDD functional style
+internal sealed record Product(
+    string Sku,
+    string Name,
+    string ImgUri,
+    decimal Price,
+    string? Description);
