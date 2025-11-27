f7411a404e1175df10e06fe5761d40e65a645e80
Dominik Pupala <dominik.pupala@gmail.com>
2025-11-27 11:11:52 +0100

    edit products read service

--- src/WebApiPatchPoC/Features/Products/Common/IProductReadService.cs
+++ src/WebApiPatchPoC/Features/Products/Common/IProductReadService.cs
@@ -3,4 +3,5 @@ namespace WebApiPatchPoC.Features.Products.Common;
 internal interface IProductReadService
 {
     Task<IReadOnlyList<ProductReadModel>> GetProducts();
+    Task<ProductReadModel?> GetProductBySku(string sku);
 }
